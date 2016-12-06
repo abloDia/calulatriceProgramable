d4118c528cb73f7c96729ab103ca49014e9e092f
sakhi <mehdi.sakhi@uha.fr>
2016-12-06 14:12:32 +0000

    creation de nombre.h && nombre.cpp

---
+++ nombre.h
@@ -0,0 +1,26 @@
+#ifndef NOMBRE_H
+#define NOMBRE_H
+
+class Nombre{
+
+public:
+	virtual Nombre* addition(Nombre* nb) const  =0;
+	virtual Nombre* multiplication(Nombre* nb) const =0;
+	virtual Nombre* division(Nombre* nb) const =0;
+	virtual Nombre* soustraction(Nombre* nb) const = 0;
+
+	virtual Nombre* sinus() const = 0;
+	virtual Nombre* cosinus() const = 0;
+	virtual Nombre* tangente() const = 0;
+
+	virtual Nombre* sinusHyperbolique() const = 0;
+	virtual Nombre* cosinusHyperbolique() const = 0;
+	virtual Nombre* tangenteHyperbolique() const = 0;
+	
+	virtual void afficherValeur() const = 0;
+
+private:
+
+};
+
+#endif
